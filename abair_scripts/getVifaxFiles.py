2dda15c32dced2834d0d1915f3c9f1fc2ebc888c
Harald Berthelsen <berthelh@tcd.ie>
2017-10-20 13:48:18 +0100

    hb

--- abair_scripts/getVifaxFiles.py
+++ abair_scripts/getVifaxFiles.py
@@ -7,3 +7,14 @@ r = requests.get(url)
 html_doc = r.text
 
 soup = BeautifulSoup(html_doc, 'html.parser')
+
+#for link in soup.find_all('a'):
+#    print(link.get('href'))
+
+for div in soup.find_all("div", class_="wp-video"):
+    print(div)
+    source = div.find("source")
+    print(source["src"])
+
+    print(div.parentnext_sibling)
+
