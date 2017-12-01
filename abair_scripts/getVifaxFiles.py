c8538d5fa244be5167d3f1643f7aed1221115cb6
Harald Berthelsen <haraldberthelsen@gmail.com>
2017-12-01 19:55:33 +0000

    hb

--- abair_scripts/getVifaxFiles.py
+++ abair_scripts/getVifaxFiles.py
@@ -7,3 +7,11 @@ r = requests.get(url)
 html_doc = r.text
 
 soup = BeautifulSoup(html_doc, 'html.parser')
+
+print(soup)
+
+print(soup.title)
+
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
